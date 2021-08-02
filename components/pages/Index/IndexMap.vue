cfcb91c60a9354d6ef0b3cb481aea1b40ac6c5ea
Sennett Lau <laub1199@gmail.com>
2021-08-03 01:40:36 +0800

    added IndexMap

---
+++ components/pages/Index/IndexMap.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="index-map">
+    <img src="/media/pages/index/map-bottom.svg" alt="bottom" width="1895" height="292" class="bottom">
+    <div class="content">
+      <div class="image-container">
+        <img src="/media/pages/index/map.png" alt="map" width="406" height="297">
+      </div>
+      <div class="text">
+        <h2>資訊無界</h2>
+        <p>１８區補習社雲集</p>
+        <p>安坐家中瀏覽毫無難度</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexMap'
+}
+</script>
+
+<style lang="scss" scoped>
+.index-map {
+  display: flex;
+  position: relative;
+  padding-bottom: 5rem;
+  @media screen and (min-width: 125rem) {
+    height: calc(377px + (100vw - 2000px) / 10);
+  }
+  .bottom {
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+  }
+  .content {
+    width: 50%;
+    display: flex;
+    justify-content: flex-end;
+    @media screen and (max-width: 85.5rem){
+      width: 100%;
+      justify-content: flex-start;
+      padding-left: 1rem;
+    }
+    .text {
+      display: flex;
+      flex-direction: column;
+      padding-top: 5rem;
+      h2 {
+        color: #343A41;
+        font-weight: bold;
+        font-size: 48px;
+        line-height: 56px;
+      }
+      p {
+        color: #343A41;
+        margin: 0;
+        font-size: 25px;
+        line-height: 29px;
+      }
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="">
     <IndexHero />
+    <IndexMap />
   </div>
 </template>
 
@@ -9,7 +10,8 @@ import { metaTag } from '~/mixin/meta_head'
 export default {
   name: 'Index',
   components: {
-    IndexHero: () => import('~/components/pages/Index/IndexHero')
+    IndexHero: () => import('~/components/pages/Index/IndexHero'),
+    IndexMap: () => import('~/components/pages/Index/IndexMap')
   },
   mixins: [
     metaTag
