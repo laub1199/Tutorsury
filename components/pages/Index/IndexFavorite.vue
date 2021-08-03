8bc22d607d7c3da3d68c9d506c4da38c2cd9f179
Sennett Lau <laub1199@gmail.com>
2021-08-03 13:19:01 +0800

    modified IndexFavorite for mobile design

--- components/pages/Index/IndexFavorite.vue
+++ components/pages/Index/IndexFavorite.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="index-favorite">
     <div class="container">
-      <div class="row">
-        <div class="col-6">
-          <div class="text">
+      <div class="row flex-column-reverse flex-md-row">
+        <div class="col-12 col-md-6 mb-4 mb-md-0">
+          <div class="text text-center text-md-left">
             <h2>精選收藏</h2>
             <p>心儀補習社任意收藏</p>
             <p>容後再次找回</p>
           </div>
         </div>
-        <div class="col-6">
+        <div class="col-12 col-md-6 text-center text-md-left">
           <img src="/media/pages/index/tutorial-center.png" alt="video" height="309" width="410">
         </div>
       </div>
@@ -36,6 +36,11 @@ export default {
     float: right;
     padding-top: 3rem;
     margin-right: 5rem;
+    @include media-breakpoint-down(sm) {
+      width: 100%;
+      margin: 0;
+      padding: 0;
+    }
     h2 {
       color: #FFFFFF;
       font-weight: bold;
@@ -47,6 +52,14 @@ export default {
       margin: 0;
       font-size: 25px;
       line-height: 29px;
+    }
+  }
+  img {
+    @include media-breakpoint-down(sm) {
+      transform: translate(50px, 0);
+    }
+    @include media-breakpoint-down(xs) {
+      transform: translate(15px, 0);
     }
   }
 }
